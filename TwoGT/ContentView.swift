aab1c467898d38e0e44a572ec5496daeb59c0491
Arthur E. Mayes, Esq <artmayes167@gmail.com>
2020-07-08 16:28:36 -0500

    Initial Commit

---
+++ TwoGT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwoGT
+//
+//  Created by Arthur Mayes on 7/8/20.
+//  Copyright © 2020 Arthur Mayes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
