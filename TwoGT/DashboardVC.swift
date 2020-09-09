94932844efa81e44778e1cd71f2f7eeb4e985e96
Arthur E. Mayes, Esq <artmayes167@gmail.com>
2020-09-09 12:00:38 -0500

    Needs loop created in DashboardVC

--- TwoGT/DashboardVC.swift
+++ TwoGT/DashboardVC.swift
@@ -27,9 +27,53 @@ class DashboardVC: UIViewController {
     }
     */
     
-    let str = String(format: "This app behaves like a normal app by default, hunting down any way to get to the interwebs, then sending and grabbing as much data as possible. \n You have the option of setting the app so that it will only communicate with the interwebs when you say so. /n Turning this setting on will enable an interface in the app that gives you full control over any networking activity performed herein. /n Hopefully this will reduce costs associated with data plans and untrusted networks.")
+    let str = String(format: "\nThis app behaves like a normal app by default, hunting down any way to get to the interwebs, then sending and grabbing as much data as possible. \n\n You have the option of setting the app so that it will only communicate with the interwebs when you say so. \n\n Turning this setting on will enable an interface in the app that gives you full control over any networking activity performed herein. \n\n Hopefully this will reduce costs associated with data plans and untrusted networks.")
 
     @IBAction func showVigilanceInfo() {
         showOkayAlert(title: "Vigilance", message: str, handler: nil)
+    }
+    
+    
+    var needsWriter = NeedsDbWriter()
+    var states: [USState] = []
+    var allStates: [String] = []
+    @IBAction func makeNeeds() {
+        
+        let path = Bundle.main.path(forResource: "citiesAndStates", ofType: "json")
+        let url = URL.init(fileURLWithPath: path!)
+        do {
+            let jsonData = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            
+            print(jsonData)
+            let container = try decoder.decode([String: [String]].self, from: jsonData) as [String: [String]]
+            print(container)
+            container.forEach { (key, value) in
+                let st = USState(name: key, cities: value)
+                states.append(st)
+                allStates.append(key)
+            }
+            allStates.sort{ $0 < $1 }
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        for s in states {
+            let c = s.cities
+            for city in c {
+                let locData = NeedsDbWriter.LocationInfo(city: city, state: s.name, country: "USA", address: nil, geoLocation: nil)
+                let cat = NeedType.allCases[Int(arc4random())%5].rawValue.capitalized
+                let need = NeedsDbWriter.NeedItem(category: cat, description: String(format: "\(c), \(s.name), \(cat)"), validUntil: 4124045393, owner: "artmayes167@gmail.com", locationInfo: locData)
+                self.needsWriter.addNeed(need, completion: { error in
+                    if error == nil {
+                        print("Need added!")
+                    } else {
+                        print("Error writing a need: \(error!)")
+                    }
+                })
+            }
+        }
+        
     }
 }
