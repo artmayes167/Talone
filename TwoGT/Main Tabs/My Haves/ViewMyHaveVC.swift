4b7c81ae22a75acc58083dfe37cef0cdce79eb72
Jyrki Hoisko <jyrki.hoisko@gmail.com>
2020-09-21 13:36:42 -0700

    [ViewMyHaveVC] Deleting Have from Core Data as well, and dismissing to previous view after deletion (Firebase; online required)

--- TwoGT/Main Tabs/My Haves/ViewMyHaveVC.swift
+++ TwoGT/Main Tabs/My Haves/ViewMyHaveVC.swift
@@ -10,7 +10,7 @@ import UIKit
 import Toast_Swift
 
 class ViewMyHaveVC: UIViewController {
-    
+
     var have: Have? {
         didSet {
             if isViewLoaded {
@@ -20,48 +20,43 @@ class ViewMyHaveVC: UIViewController {
     }
 
     @IBOutlet weak var headerTitleLabel: UILabel!
-
     @IBOutlet weak var scrollView: UIScrollView!
-    
     @IBOutlet weak var locationLabel: UILabel!
-    
     @IBOutlet weak var doYouLabel: UILabel!
-    
     @IBOutlet weak var needDescriptionTextView: UITextView!
     @IBOutlet weak var personalNotesTextView: UITextView!
-    
+
     // MARK: - IBActions
     @IBAction func deleteHave(_ sender: Any) {
         deleteCurrentHave()
     }
-    
+
      // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Notifications
-            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name:UIResponder.keyboardWillShowNotification, object: nil)
-            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name:UIResponder.keyboardWillHideNotification, object: nil)
-        
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+
         if let owner = have?.haveItem?.owner ?? AppDelegate.user().handle {
             headerTitleLabel.text = String(format: "%@'s Have", owner)
         }
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         populateUI()
-        
     }
-    
+
     func populateUI() {
         guard let c = have?.haveItem?.category, let cityState = have?.purpose?.cityState else { return }
-        locationLabel.text = String(format:"%@ in %@", c, cityState.displayName())
+        locationLabel.text = String(format: "%@ in %@", c, cityState.displayName())
         view.layoutIfNeeded()
     }
-    
+
     // MARK: - Keyboard Notifications
-    @objc func keyboardWillShow(notification: NSNotification){
+    @objc func keyboardWillShow(notification: NSNotification) {
         let userInfo = notification.userInfo!
         var keyboardFrame: CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
         keyboardFrame = self.view.convert(keyboardFrame, from: nil)
@@ -71,20 +66,21 @@ class ViewMyHaveVC: UIViewController {
         scrollView.contentInset = contentInset
     }
 
-    @objc func keyboardWillHide(notification: NSNotification){
+    @objc func keyboardWillHide(notification: NSNotification) {
         let contentInset: UIEdgeInsets = UIEdgeInsets()
         scrollView.contentInset = contentInset
     }
 
     private func deleteCurrentHave() {
         guard let haveItem = have?.haveItem else { return }
-                
+
+        have?.deleteHave()
+
         HavesDbWriter().deleteHave(id: haveItem.id!, creator: haveItem.createdBy ?? "") { error in
             if error == nil {
-                self.view.makeToast("You have Deleted the Have", duration: 1.0, position: .center) {_ in 
-                    self.dismiss(animated: true)
+                self.view.makeToast("You have Deleted the Have", duration: 1.0, position: .center) {_ in
+                    self.performSegue(withIdentifier: "dismissToMyHaves", sender: self)
                 }
-                // TODO: ART: ADD HERE THE CODE TO REMOVE THIS HAVE FROM COREDATA!
             } else {
                 self.showOkayAlert(title: "Error", message: "Error while deleting have. Error: \(error!.localizedDescription)", handler: nil)
             }
