9af926ad15ce2ae656191218c19e5268dd730589
Jyrki Hoisko <jyrki.hoisko@gmail.com>
2020-09-21 13:36:42 -0700

    [ViewMyHaveVC] Deleting Have from Core Data as well, and dismissing to previous view after deletion (Firebase; online required)

--- TwoGT/Data Structures/Core Data/CoreDataObjectExtensions.swift
+++ TwoGT/Data Structures/Core Data/CoreDataObjectExtensions.swift
@@ -10,20 +10,20 @@ import UIKit
 import CoreData
 
 extension Purpose {
-    
+
     class func create(type: String, city: String, state: String, country: String = "USA", community: String = "") -> Purpose {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError() }
-        
+
         let managedContext = appDelegate.persistentContainer.viewContext
-        
+
         let entity = NSEntityDescription.entity(forEntityName: "Purpose",
                                      in: managedContext)!
-        
+
        let purpose = Purpose(entity: entity, insertInto: managedContext)
         purpose.setValue(type, forKey: "category")
         let c = CityState.create(city: city, state: state, country: country, communityName: community)
         purpose.setValue(c, forKey: "cityState")
-        
+
         do {
           try managedContext.save()
             return purpose
@@ -32,7 +32,7 @@ extension Purpose {
             fatalError()
         }
     }
-    
+
     func cdKey() -> String? {
         return cityState?.displayName()
     }
@@ -41,21 +41,21 @@ extension Purpose {
 extension CityState {
     class func create(city: String, state: String, country: String, communityName: String) -> CityState {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError() }
-        
+
         let managedContext = appDelegate.persistentContainer.viewContext
-        
+
         let entity = NSEntityDescription.entity(forEntityName: "CityState",
                                      in: managedContext)!
-        
+
         let cityState = CityState(entity: entity, insertInto: managedContext) as CityState
-        
+
         let community = Community.create(communityName: communityName)
-        
+
         cityState.country = country
         cityState.city = city
         cityState.state = state
         cityState.addToCommunities(community)
-        
+
         do {
           try managedContext.save()
             return cityState
@@ -64,13 +64,13 @@ extension CityState {
             fatalError()
         }
     }
-    
+
     func displayName() -> String {
         if let c = city, let s = state {
             return c.capitalized + ", " + s.capitalized
         } else { fatalError() }
     }
-    
+
     func locationInfo() -> AppLocationInfo {
         let a = AppLocationInfo.create(city: city!, state: state!, country: country!)
         return a
@@ -80,19 +80,19 @@ extension CityState {
 extension Community {
     class func create(communityName: String) -> Community {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError() }
-        
+
         let managedContext = appDelegate.persistentContainer.viewContext
-        
+
         let entity = NSEntityDescription.entity(forEntityName: "Community",
                                      in: managedContext)!
-        
+
        guard let community = NSManagedObject(entity: entity,
                                               insertInto: managedContext) as? Community else {
                                                 fatalError()
         }
-        
+
         community.name = communityName
-        
+
         do {
           try managedContext.save()
             return community
@@ -104,14 +104,13 @@ extension Community {
 }
 
 extension User {
-    func sortedAddresses() -> Dictionary<String, [SearchLocation]> {
-        var dict: Dictionary<String, [SearchLocation]> = [:]
+    func sortedAddresses() -> [String: [SearchLocation]] {
+        var dict: [String: [SearchLocation]] = [:]
         if let locs = searchLocations {
             var home: [SearchLocation] = []
             var alternate: [SearchLocation] = []
             for s in locs {
-                if (s as? SearchLocation)?.type == "home" { home.append(s as! SearchLocation) }
-                else if (s as? SearchLocation)?.type == "alternate" { alternate.append(s as! SearchLocation) }
+                if (s as? SearchLocation)?.type == "home" { home.append(s as! SearchLocation) } else if (s as? SearchLocation)?.type == "alternate" { alternate.append(s as! SearchLocation) }
             }
             if !home.isEmpty { dict["home"] = home }
             if !alternate.isEmpty { dict["alternate"] = alternate }
@@ -123,20 +122,20 @@ extension User {
 extension Email {
     class func create(name: String, emailAddress: String) -> Email {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError() }
-        
+
         let managedContext = appDelegate.persistentContainer.viewContext
-        
+
         let entity = NSEntityDescription.entity(forEntityName: "Email",
                                      in: managedContext)!
-        
+
        guard let email = NSManagedObject(entity: entity,
                                               insertInto: managedContext) as? Email else {
                                                 fatalError()
         }
-        
+
         email.name = name
         email.emailString = emailAddress
-        
+
         do {
           try managedContext.save()
             return email
@@ -150,8 +149,7 @@ extension Email {
 extension Item {
     func areAllRequiredFieldsFilled(light: Bool) -> Bool {
         guard let c = category, !c.isEmpty else { return false }
-        if light { return true }
-        else {
+        if light { return true } else {
             return !(desc?.isEmpty ?? true) && !(headline?.isEmpty ?? true)
         }
     }
@@ -159,14 +157,14 @@ extension Item {
 
 extension SearchLocation {
     class func createSearchLocation(city: String, state: String, country: String = "USA", community: String = "") -> SearchLocation {
-        
+
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError() }
-        
+
         let managedContext = appDelegate.persistentContainer.viewContext
-        
+
         let entity = NSEntityDescription.entity(forEntityName: "SearchLocation",
                                      in: managedContext)!
-        
+
        guard let searchLocation = NSManagedObject(entity: entity,
                                               insertInto: managedContext) as? SearchLocation else {
                                                 fatalError()
@@ -183,7 +181,7 @@ extension SearchLocation {
             fatalError()
         }
     }
-    
+
     func displayName() -> String {
         if let c = city, let s = state {
             return c.capitalized + ", " + s.capitalized
@@ -194,12 +192,12 @@ extension SearchLocation {
 extension Address {
     class func createAddress(city: String, state: String, country: String = "USA", type: String = "home") -> Address {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError() }
-        
+
         let managedContext = appDelegate.persistentContainer.viewContext
-        
+
         let entity = NSEntityDescription.entity(forEntityName: "Address",
                                      in: managedContext)!
-        
+
        guard let address = NSManagedObject(entity: entity,
                                               insertInto: managedContext) as? Address else {
                                                 fatalError()
@@ -213,7 +211,7 @@ extension Address {
             fatalError()
         }
     }
-    
+
     func locationInfoOrNil() -> AppLocationInfo? {
         if let _ = city, let _ = state, let _ = country {
             return self
@@ -225,12 +223,12 @@ extension Address {
 extension NeedItem {
     class func createNeedItem(item: NeedsBase.NeedItem) -> NeedItem {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError() }
-        
+
         let managedContext = appDelegate.persistentContainer.viewContext
-        
+
         let entity = NSEntityDescription.entity(forEntityName: "NeedItem",
                                      in: managedContext)!
-        
+
        guard let needItem = NSManagedObject(entity: entity,
                                               insertInto: managedContext) as? NeedItem else {
                                                 fatalError()
@@ -255,12 +253,12 @@ extension NeedItem {
 extension Need {
     class func createNeed(item: NeedItem) -> Need {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError() }
-        
+
         let managedContext = appDelegate.persistentContainer.viewContext
-        
+
         let entity = NSEntityDescription.entity(forEntityName: "Need",
                                      in: managedContext)!
-        
+
        guard let need = NSManagedObject(entity: entity,
                                               insertInto: managedContext) as? Need else {
                                                 fatalError()
@@ -279,12 +277,12 @@ extension Need {
 extension HaveItem {
     class func createHaveItem(item: HavesBase.HaveItem) -> HaveItem {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError() }
-        
+
         let managedContext = appDelegate.persistentContainer.viewContext
-        
+
         let entity = NSEntityDescription.entity(forEntityName: "HaveItem",
                                      in: managedContext)!
-        
+
        guard let haveItem = NSManagedObject(entity: entity,
                                               insertInto: managedContext) as? HaveItem else {
                                                 fatalError()
@@ -304,17 +302,18 @@ extension HaveItem {
             fatalError()
         }
     }
+
 }
 
 extension Have {
     class func createHave(item: HaveItem) -> Have {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError() }
-        
+
         let managedContext = appDelegate.persistentContainer.viewContext
-        
+
         let entity = NSEntityDescription.entity(forEntityName: "Have",
                                      in: managedContext)!
-        
+
        guard let have = NSManagedObject(entity: entity,
                                               insertInto: managedContext) as? Have else {
                                                 fatalError()
@@ -328,23 +327,32 @@ extension Have {
             fatalError()
         }
     }
+
+    func deleteHave() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError() }
+
+        let managedContext = appDelegate.persistentContainer.viewContext
+        managedContext.delete(self)
+        try? managedContext.save()
+    }
+
 }
 
 extension AppLocationInfo {
     class func create(city: String, state: String, country: String) -> AppLocationInfo {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError() }
-        
+
         let managedContext = appDelegate.persistentContainer.viewContext
-        
+
         let entity = NSEntityDescription.entity(forEntityName: "AppLocationInfo",
                                      in: managedContext)!
-        
+
         let locationInfo = AppLocationInfo(entity: entity, insertInto: managedContext)
-        
+
         locationInfo.country = country
         locationInfo.city = city
         locationInfo.state = state
-        
+
         do {
           try managedContext.save()
             return locationInfo
