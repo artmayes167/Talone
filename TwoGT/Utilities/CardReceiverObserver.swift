2fb65eaf66221caf94ad8280b778a64da5954d4b
Jyrki Hoisko <jyrki.hoisko@gmail.com>
2020-09-30 21:17:39 -0700

    [Card observation] Showing a toast on ANY VC after receiving updated / new Cards; Still doesn't store card to CD

--- TwoGT/Utilities/CardReceiverObserver.swift
+++ TwoGT/Utilities/CardReceiverObserver.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 class CardReceiverObserver {
 
@@ -43,26 +44,69 @@ class CardReceiverObserver {
                 }
                 //interaction.receivedCard.deleteCard()
             }
+            self.notifyUserOfNewAndModifiedCards(newCards, modifiedCards)
         }
     }
 
-    private func notifyUserOfNewCards(_ fibCards: [CardsBase.FiBCardItem]) {
-        guard fibCards.count > 0 else { return }
+    private func notifyUserOfNewAndModifiedCards(_ newCards: [CardsBase.FiBCardItem], _ modifiedCards: [CardsBase.FiBCardItem]) {
+        guard newCards.count > 0 || modifiedCards.count > 0 else { return }
 
-        let numbers = ["two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+        let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
         var str = ""
 
-        switch fibCards.count {
+        switch newCards.count {
+        case 0:
+            switch modifiedCards.count {
+            case 1:
+                str = "\(modifiedCards[0].owner) modified his/her card."
+            case 2:
+                str = "\(modifiedCards[0].owner) & \(modifiedCards[1].owner) modified their cards."
+            default:
+                let s = modifiedCards.count-2 < 11 ? numbers[modifiedCards.count-2] : "\(modifiedCards.count-2)" // "nine", "ten", "11", "12",...
+                str = "\(modifiedCards[0].owner), \(modifiedCards[1].owner) and \(s) other users modified their cards."
+            }
         case 1:
-            str = "\(fibCards[0].owner) sent you a new card."
+            switch modifiedCards.count {
+            case 0:
+                str = "\(newCards[0].owner) sent you a new card."
+            case 1:
+                str = "\(newCards[0].owner) sent you a new card, \(modifiedCards[0].owner) modified his/her card."
+            case 2:
+                str = "\(newCards[0].owner) sent you a new card, \(modifiedCards[0].owner) & \(modifiedCards[1].owner) modified their cards."
+            default:
+                let s = modifiedCards.count-2 < 11 ? numbers[modifiedCards.count-2] : "\(modifiedCards.count-2)" // "nine", "ten", "11", "12",...
+                str = "\(newCards[0].owner) sent you a new card, \(s) users modified their cards."
+            }
         case 2:
-            str = "\(fibCards[0].owner) and \(fibCards[1].owner) sent you their cards."
+            switch modifiedCards.count {
+            case 0:
+                str = "\(newCards[0].owner) and \(newCards[1].owner) sent you their cards."
+            case 1:
+                str = "\(newCards[0].owner) and \(newCards[1].owner) sent you their cards, \(modifiedCards[0].owner) modified his/her card."
+            default:
+                let s = modifiedCards.count-2 < 11 ? numbers[modifiedCards.count-2] : "\(modifiedCards.count-2)" // "nine", "ten", "11", "12",...
+                str = "\(newCards[0].owner) and \(newCards[1].owner) sent you their cards, \(s) users modified their cards."
+            }
         default:
-            let s = fibCards.count-2 < 8 ? numbers[fibCards.count-2] : "\(fibCards.count-2)" // "nine", "ten", "11", "12",...
-            str = "\(fibCards[0].owner), \(fibCards[1].owner) and \(s) others sent you their cards."
+            let s = newCards.count-2 < 11 ? numbers[newCards.count-2] : "\(newCards.count-2)" // "nine", "ten", "11", "12",...
+            let s2 = modifiedCards.count < 11 ? numbers[modifiedCards.count] : "\(modifiedCards.count)" // "nine", "ten", "11", "12",...
+
+            switch modifiedCards.count {
+            case 0:
+                str = "\(newCards[0].owner), \(newCards[1].owner) and \(s) others sent you their cards."
+            case 1:
+                str = "\(modifiedCards[0].owner) modified his/her card, \(newCards[0].owner), \(newCards[1].owner) and \(s) others sent you their cards."
+            default:
+                str = "\(newCards[0].owner), \(newCards[1].owner) and \(s) others sent you their cards. \(s2) users modified their cards."
+            }
         }
         // Show Toast
-        //self.view.makeToast(str, duration: 2.0, position: .top) {_ in
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError() }
+        let vc = appDelegate.window?.rootViewController
+        let duration = newCards.count + modifiedCards.count >= 3 ? 3.0 : 2.0 // increase toast time if lot's of text to show.
+        vc?.view.makeToast(str, duration: duration, position: .top) {_ in
+            print("Toast done!")
+        }
     }
 }
 
