d08fa2c78098a30026276a8d98da1596ffc1a52a
Jyrki Hoisko <jyrki.hoisko@gmail.com>
2020-10-01 17:32:41 -0700

    [Card observation] Showing a toast on any VC after receiving updated / new Cards; Received Cards stored to CD and immediately discarded from FiB.

--- TwoGT/Utilities/CardReceiverObserver.swift
+++ TwoGT/Utilities/CardReceiverObserver.swift
@@ -16,50 +16,43 @@ class CardReceiverObserver {
     func startObserving() {
         observeCardReceptions()
     }
-    
-    // TODO: - Jyrki will look at GateKeeper().decodeCodableInstance(data: Data) -> CardTemplateInstance and GateKeeper().buildCodableInstanceAndEncode(instance: CardTemplateInstance) -> Data
 
     func observeCardReceptions() {
-        
-//        CardsFetcher().observeCardsSentToMe { [self] fibCardItems in
+
         fetcher.observeCardsSentToMe { (fibCardItems: [CardsBase.FiBCardItem]) in
-//            print("cards received! \(fibCardItems)")
-//            let interactions: [Interaction] = AppDelegate.user.interactions
-//            var newCards = [CardsBase.FiBCardItem]()
-//            var modifiedCards = newCards
-//
-//            // cross-reference Cards
-//
-//            checkNew: for fibCard in fibCardItems {
-//                for interaction in interactions where fibCard.id == interaction.receivedCard?.uid {
-//                    // card modified?
-//                    modifiedCards.append(fibCard)
-//                    continue checkNew
-//                }
-//                newCards.append(fibCard)
-//                // Create card in CD
-//            }
-//
-//            // Search for any card that has been deleted.
-//            checkDeleted: for interaction in interactions {
-//                for fibCard in fibCardItems where fibCard.id == interaction.receivedCard?.uid {
-//                    continue checkDeleted
-//                }
-//                //interaction.receivedCard.deleteCard()
-//            }
-//            self.notifyUserOfNewAndModifiedCards(newCards, modifiedCards)
-            
-            // New stuff
+            let interactions: [Interaction] = AppDelegate.user.interactions
+            var newCards = [CardsBase.FiBCardItem]()
+            var modifiedCards = newCards
+
+            // cross-reference Cards received from FiB vs. cards in CD
             for fibCard in fibCardItems {
-                let decodedData = Data(base64Encoded: fibCard.payload)!
-                _ = GateKeeper().decodeCodableInstance(data:decodedData)
+                if let decodedData = Data(base64Encoded: fibCard.payload) {
+                    // Store the received card
+                    _ = GateKeeper().decodeCodableInstance(data: decodedData)
+                    // Ask server database to discard it immediately. Effectively this means
+                    // no data can be restored after app uninstall / moving to another phone.
+                    // DeleteCard() has a completion handler, but currently unused.
+                    CardsDbWriter.deleteCard(id: fibCard.id!)
+                } else {
+                    print("WARNING: \(#function) Payload decoding failed for card: \(fibCard.id!)")
+                    continue
+                }
+
+                var isExisting = false
+                for interaction in interactions where fibCard.owner == interaction.referenceUserHandle {
+                    modifiedCards.append(fibCard)
+                    isExisting = true
+                }
+                if !isExisting { newCards.append(fibCard) }
             }
-            DispatchQueue.main.async {
-                guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError() }
-                let vc = appDelegate.window!.rootViewController
-                vc?.view.makeToast("new contact information received from \(fibCardItems.count) contacts")
-            }
-            
+
+            // NOTE: Deleted cards will not be noted; Sender needs to send "an empty" card to
+            // nullify the previously sent card. This is regarded as sender-originated deletion
+            // of the card.
+
+            // Nofity user who sent their cards (new / modified). This could be large set
+            // after long offline period.
+            self.notifyUserOfNewAndModifiedCards(newCards, modifiedCards)
         }
     }
 
@@ -125,62 +118,3 @@ class CardReceiverObserver {
         }
     }
 }
-
-/*
- HavesDbFetcher().observeMyHaves { [self] fibHaveItems in
-
-     var addedNeedOwners = [String]()
-     var changedHaves = [HaveItem]()
-     var isRedrawRequired = false
-
-     // Cross-reference needs
-     for have in self.haves {
-         for fibHave in fibHaveItems where fibHave.id == have.haveItem?.id {
-             if let needStubs = fibHave.needs, let haveItem = have.haveItem {
-                 var isChanged = false
-                 changedHaves.append(haveItem)   // for showing on UI
-
-                 let cdNeeds = have.childNeeds
-
-                 // First determine if there are any new needStubs that are missing from CD
-                 // These are the people that have linked with this have.
-                 for needStub in needStubs {
-                     var found = false
-                     for cdNeed in cdNeeds where cdNeed.needItem?.id == needStub.id {
-                         found = true
-                         break
-                     }
-                     if found == false {
-                         // create a new need (gets appended to childItems implicitly)
-                         var fibNeed = NeedsBase.NeedItem(category: fibHave.category, validUntil: fibHave.validUntil!, owner: needStub.owner, createdBy: needStub.createdBy, locationInfo: fibHave.locationInfo).self
-                         fibNeed.id = needStub.id // overwrite the implicit Id to reflect existing id.
-                         let n = Need.createNeed(item: NeedItem.createNeedItem(item: fibNeed))
-                         n.parentHaveItemId = fibHave.id
-                         addedNeedOwners.append(fibNeed.owner)
-                         isChanged = true
-                     }
-                 }
-
-                 // Then determine if there are needStubs being deleted requiring cleanup from CD.
-                 // These are the people that have removed the link with this have.
-                 for cdNeed in cdNeeds {
-                     var found = false
-                     for needStub in needStubs where needStub.id == cdNeed.needItem?.id {
-                         found = true
-                         break
-                     }
-                     if found == false {
-                         cdNeed.deleteNeed()
-                         isChanged = true
-                     }
-                 }
-                 if isChanged { haveItem.update(); isRedrawRequired = true } // store changes to CD
-             }
-         }
-     }
-     if isRedrawRequired { collectionView.reloadData() }
-     notifyUserOfNewLinks(addedNeedOwners, changedHaves)
- }
-}
-
- */
