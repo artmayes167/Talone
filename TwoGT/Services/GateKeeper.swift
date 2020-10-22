2d2a967543a588392165359a6d0c7d6e08a80198
Arthur E. Mayes, Esq <artmayes167@yahoo.com>
2020-10-22 09:54:08 -0500

    File restructure

--- TwoGT/Services/GateKeeper.swift
+++ TwoGT/Services/GateKeeper.swift
@@ -9,6 +9,23 @@
 import Foundation
 import CoreData
 
+// MARK: - Helper functions for creating encoders and decoders
+public func newJSONDecoder() -> JSONDecoder {
+    let decoder = JSONDecoder()
+    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+        decoder.dateDecodingStrategy = .iso8601
+    }
+    return decoder
+}
+
+public func newJSONEncoder() -> JSONEncoder {
+    let encoder = JSONEncoder()
+    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
+        encoder.dateEncodingStrategy = .iso8601
+    }
+    return encoder
+}
+
 typealias GateKeeper = CodableCardTemplateInstanceManager
 
 class CodableCardTemplateInstanceManager {
